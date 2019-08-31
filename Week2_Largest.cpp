9cefca864a8517e2db01a42991a45cf9ff2bf9e2
sooraju99 <47816525+sooraju99@users.noreply.github.com>
2019-08-31 09:47:50 +0530

    Update Week2_Largest.cpp

--- Week2_Largest.cpp
+++ Week2_Largest.cpp
@@ -1,95 +1,31 @@
-#include <iostream>
-#include <chrono> 
+#include<iostream>
+#include<algorithm>
 using namespace std;
-using namespace std::chrono; 
-void selection_sort (int A[ ], int n , int k) {
-		cout<<"Selection sort:"<<endl<<endl;
-        int maximum;    
-		auto start = high_resolution_clock::now(); 
-        for(int i = 0; i < n-1 ; i++)  {
-             maximum = i ;
-            for(int j = i+1; j < n ; j++ ) {
-                if(A[ j ] > A[ maximum ])  {         
-                maximum = j ;
-                }
-             }
-		int temp = A[i];
-		A[i]=A[maximum];
-		A[maximum]=temp;
-        }
-		auto stop = high_resolution_clock::now();
-		for(int i=0;i<n;i++)
-		{
-			cout<<A[i]<<",";
+int main(){
+	int n;
+	cout<<"Enter the size of an array :\n";
+	cin>>n;
+	int arr[20];
+	cout<<"Enter elements of an array:\n";
+	for(int i=0;i<n;i++)	
+		cin>>arr[i];
+	cout<<"Enter the kth value:\n";
+	int k;
+	cin>>k;
+	if(k<=0){
+		cout<<"k cannot be less than or equal to zero";
+	}
+	int max;
+	for(int i=0;i<k;i++){
+		max = i;
+		for(int j=i+1;j<n;j++){
+			if(arr[j]>arr[max]){
+				swap(arr[j],arr[max]);
+			}
 		}
-		
-		cout<<endl;
-		cout<<"The largest elements upto k are: ";
-		for(int i=0;i<k;i++)
-		{
-			
-				cout<<A[i]<<",";
-			
-		
-   }
-   
-    std::chrono::duration<double, std::milli> fp_ms = stop - start;
+	}
+	for(int i=0;i<k;i++){
+		cout<<arr[i]<<" ";
+	}
 	
-	cout<<endl<< "Time taken is "<<  fp_ms.count() <<" milliseconds"<<endl;
-}
-
-void bubble_sort (int A[ ], int n , int k) {
-		cout<<endl<<"Bubble sort:"<<endl<<endl;
-		int i;
-		auto start = high_resolution_clock::now(); 
-{
-	for(int j=0;j<n-1-i;j++)
-		{
-			if(A[j+1]>A[j])
-				{	int temp=A[j];
-					A[j]=A[j+1];
-					A[j+1]=temp;
-				}
-		}
-}
-		auto stop = high_resolution_clock::now()
-		for(int i=0;i<n;i++)
-		{
-			cout<<A[i]<<",";
-		}
-		
-		cout<<endl;
-		cout<<"The largest elements upto k are: ";
-		for(int i=0;i<k;i++)
-		{
-			
-				cout<<A[i]<<",";
-			
-		
-   }
-   
-    std::chrono::duration<double, std::milli> fp_ms = stop - start;
-	
-	cout<<endl<< "Time taken is "<<  fp_ms.count() <<" milliseconds"<<endl;
-}
-   
-   
-int main()
-
-{		int n;
-		cout<<"Enter the array size:"<<endl;
-		cin>>n;
-		int A[n];
-		cout<<"Enter the array elements:"<<endl;
-		for(int i = 0; i<n;i++)
-		{
-			cin>>A[i];
-		}
-	
-		int k;
-		cout<<"Enter the kth position you want"<<endl;
-		cin>>k;
-		selection_sort(A,n,k);
-		bubble_sort(A,n,k);
-
-}
+}	
